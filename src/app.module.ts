99efed9bd4001325dce24e1072a6064d756b2023
Vlad Omelyanenko <47278133+worldofakk@users.noreply.github.com>
2020-11-23 20:56:37 +0200

    added connect to MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { GameProductsModule } from "./game-products/game-products.module";
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { MongooseModule } from "@nestjs/mongoose";
+import { URI } from "./game-products//dbConfig/db.uri";
 
 @Module({
-  imports: [GameProductsModule],
+  imports: [GameProductsModule, MongooseModule.forRoot(URI)],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/game-products/game-products.module.ts
+++ src/game-products/game-products.module.ts
@@ -1,9 +1,15 @@
 import { GameProductsController } from "./game-products.controller";
 import { ProductsService } from "./game-products.service";
 import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+
+import { Product, ProductSchema } from "./schemas/game-product.schema";
 
 @Module({
   providers: [ProductsService],
   controllers: [GameProductsController],
+  imports: [
+    MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
+  ],
 })
 export class GameProductsModule {}
